4367b3a5adc85e2cd5ffd20afa001d2ed3625984
Aaron Kling <webgeek1234@gmail.com>
2019-08-28 13:44:25 -0500

    Add ueventd conf
    Ardbeg ueventd conf is common across the platform
    
    Change-Id: I8fcc013cc55152701c49ff62c2222c96552b2608

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -7,3 +7,11 @@ LOCAL_SRC_FILES            := init.t124.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.ardbeg.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ueventd.ardbeg.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
